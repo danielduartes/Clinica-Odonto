17859e7833968d894567969ec605200767e23b05
Agamedes <agamedesrmeira@gmail.com>
2025-11-28 12:55:03 -0300

    Att Tables Clientes & Secretarios

--- admin/clientes-altera.php
+++ admin/clientes-altera.php
@@ -1,20 +1,41 @@
 <?php
-    require_once 'config.inc.php';
+require_once 'config.inc.php';
 
-    $id = $_POST['id'];
-    $nome = $_POST['cliente'];
-    $cidade = $_POST['cidade'];
-    $estado = $_POST['estado'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $sql = "UPDATE clientes SET cliente = '$nome',cidade = '$cidade',
-            estado = '$estado'
+    $id            = $_POST['id'];
+    $nome_completo = $_POST['nome_completo'];
+    $cpf           = $_POST['cpf'];
+    $email         = $_POST['email'];
+    $telefone      = $_POST['telefone'];
+    $endereco      = $_POST['endereco'];
+    $cidade        = $_POST['cidade'];
+    $estado        = $_POST['estado'];
+    $sexo          = $_POST['sexo'];
+
+    $sql = "UPDATE clientes SET 
+                nome_completo = '$nome_completo',
+                cpf           = '$cpf',
+                email         = '$email',
+                telefone      = '$telefone',
+                endereco      = '$endereco',
+                cidade        = '$cidade',
+                estado        = '$estado',
+                sexo          = '$sexo'
             WHERE id = '$id'";
 
+    $resultado = mysqli_query($conexao, $sql);
 
-    if($resultado = mysqli_query($conexao, $sql)){
-        echo "<br><h2>Cliente alterado com sucesso!";
+    if ($resultado) {
+        echo "<br><h2>Cliente alterado com sucesso!</h2>";
         echo "<a href='?pg=clientes-admin'>Voltar</a>";
-    }else{
+    } else {
         echo "<br><h3>Erro ao alterar cliente</h3>";
-        echo "<a href='?pg=clientes-admin'>Voltar</a>";
+        echo mysqli_error($conexao);
+        echo "<br><a href='?pg=clientes-admin'>Voltar</a>";
     }
+
+} else {
+    echo "<h2>Método não permitido!</h2>";
+}
+?>
